80b436ab02279c9f38ad085de74092ec91c8b3de
darshan <darshan@itfuturz.com>
2025-10-04 12:42:50 +0530

    dashboard change

--- src/pages/Dashboard/Home.tsx
+++ src/pages/Dashboard/Home.tsx
@@ -1,7 +1,7 @@
 import EcommerceMetrics from "../../components/ecommerce/EcommerceMetrics";
-import MonthlySalesChart from "../../components/ecommerce/MonthlySalesChart";
-import MonthlyTarget from "../../components/ecommerce/MonthlyTarget";
-import RecentOrders from "../../components/ecommerce/RecentOrders";
+// import MonthlySalesChart from "../../components/ecommerce/MonthlySalesChart";
+// import MonthlyTarget from "../../components/ecommerce/MonthlyTarget";
+// import RecentOrders from "../../components/ecommerce/RecentOrders";
 import DemographicCard from "../../components/ecommerce/DemographicCard";
 import PageMeta from "../../components/common/PageMeta";
 
@@ -9,27 +9,27 @@ export default function Home() {
   return (
     <>
       <PageMeta
-        title="Prime Health "
+        title="Prime Health"
         description="Prime Health Admin Dashboard"
       />
       <div className="grid grid-cols-12 gap-4 md:gap-6">
         <div className="col-span-12 space-y-6 xl:col-span-7">
           <EcommerceMetrics />
 
-          <MonthlySalesChart />
+          {/* <MonthlySalesChart /> */}
         </div>
 
-        <div className="col-span-12 xl:col-span-5">
+        {/* <div className="col-span-12 xl:col-span-5">
           <MonthlyTarget />
-        </div>
+        </div> */}
 
         <div className="col-span-12 xl:col-span-5">
           <DemographicCard />
         </div>
-
+{/* 
         <div className="col-span-12 xl:col-span-7">
           <RecentOrders />
-        </div>
+        </div> */}
       </div>
     </>
   );
